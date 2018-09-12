bebc5bd049b9829639c583edb872b680140fbc43
popliarman2019 <42761596+popliarman2019@users.noreply.github.com>
2018-09-11 21:14:42 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("l(a\n\n\nle\naf\nfa\n\n\nl\n\n\ns\none\nl\n\n\niness");
+
+    }
+}
