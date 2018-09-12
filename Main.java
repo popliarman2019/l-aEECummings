fcb2acde41cf76fcb5c253f43cf157224d589a12
popliarman2019 <42761596+popliarman2019@users.noreply.github.com>
2018-09-12 09:13:28 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("l(a\n\n\nle\naf\nfa\n\n\nl\n\n\ns\none\nl\n\n\niness");
+        System.out.println("l(a\n\nle\naf\nfa\n\nl\n\ns\none\nl\n\niness");
 
     }
 }
